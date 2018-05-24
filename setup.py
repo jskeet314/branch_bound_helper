533cd83748a27bb44d6d291216901685eea839c1
John Skeet <ferrisyang16@gmail.com>
2018-05-24 03:31:36 -0700

    setup.py correct info

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-  name = 'mypackage',
-  packages = ['mypackage'], # this must be the same as the name above
+  name = 'branch_bound_package',
+  packages = ['branch_bound_package'],
   version = '0.1',
   description = 'A library for assisting branch and bound operations',
   author = 'John Skeet',
-  author_email = 'peterldowns@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  author_email = 'jskeet314@gmail.com',
+  url = 'https://github.com/jskeet314/branch_bound_helper',
+  download_url = 'https://github.com/jskeet314/branch_bound_helper/archive/0.1.tar.gz',
+  keywords = ['testing', 'logging', 'example'], 
   classifiers = [],
 )
